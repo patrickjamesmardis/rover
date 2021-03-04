15f71de55850ecff3559f77b9aede10553b7bed7
Patrick James Mardis <59069682+patrickjamesmardis@users.noreply.github.com>
2021-03-04 18:41:04 -0500

    npm init

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist/js")
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ["style-loader", "css-loader", "sass-loader"]
+        }]
+    }
+}
